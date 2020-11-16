5a742bf81ae6a68a40d3a8d8ce79a9f37a22980c
stevan <stevann.tosic@gmail.com>
2020-11-16 21:34:07 +0100

    add seeder for students and classrooms

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(ClassroomTableSeeder::class);
+        $this->call(StudentTableSeeder::class);
     }
 }
